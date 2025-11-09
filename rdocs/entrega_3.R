4d917130062e00ec467480a07f6442d81325c597
luisaf-s <luisafs3103@gmail.com>
2025-11-09 19:07:36 -0300

    Entrega final amem 2

--- rdocs/entrega_3.R
+++ rdocs/entrega_3.R
@@ -65,15 +65,23 @@ media_por_ano <- real %>%
 
 grafico_linhas <- ggplot(media_por_ano) +
   aes(x=Date, y=media_real, group=1) +
-  geom_line(size=1,colour="#A11D21") + geom_point(colour="#A11D21",
+  geom_line(linewidth=1,colour="#A11D21") + geom_point(colour="#A11D21",
                                                   size=2) +
-  labs(x="Ano", y="Média") +
+  labs(x="Ano", y="Receita média em reais") +
   scale_x_continuous(breaks = 1880:1889, limits = c(1880,1889))
 theme_estat()
 ggsave("series_uni.pdf", width = 158, height = 93, units = "mm")
 
-grafico_linhas
+grafico_linhas2 <- ggplot(media_por_ano) +
+  aes(Date, media_real, group=1) +
+  geom_line(linewidth=1,colour="#A11D21") + 
+  geom_point(colour="#A11D21", size=2) +
+  labs(x="Ano", y="Receita Média em Reais") +
+  theme_estat() +
+  scale_x_continuous(breaks = scales::pretty_breaks(n =10))
 
+grafico_linhas
+grafico_linhas2
 
 rm(infos_clientes)
 infos_clientes <- read_excel("relatorio_old_town_road.xlsx", sheet = "infos_clientes")
@@ -184,6 +192,79 @@ resumo_idade <- cidade %>%
   )
 
 resumo_idade
+print_quadro_resumo <- function(data, var_name, title = "Medidas resumo da(o) [nome da variável]", label = "quad:quadro_resumo1") {
+  var_name <- substitute(var_name)
+  
+  data <- data %>%
+    group_by(group_var = data[[deparse(var_name)]]) %>%
+    summarize(
+      Média = round(mean(!!sym(deparse(var_name))), 2),
+      "Desvio Padrão" = round(sd(!!sym(deparse(var_name))), 2),
+      Variância = round(var(!!sym(deparse(var_name))), 2),
+      Mínimo = round(min(!!sym(deparse(var_name))), 2),
+      "1º Quartil" = round(quantile(!!sym(deparse(var_name)), probs = .25), 2),
+      Mediana = round(quantile(!!sym(deparse(var_name)), probs = .5), 2),
+      "3º Quartil" = round(quantile(!!sym(deparse(var_name)), probs = .75), 2),
+      Máximo = round(max(!!sym(deparse(var_name))), 2)
+    ) %>%
+    t() %>%
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep = "")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  
+  latex <- str_c(latex, "| l |\n", sep = " ")
+  for (i in seq(2, col_count)) {
+    numCount <- data[i, -c(1)] %>%
+      as.numeric() %>%
+      {floor(log10(.)) + 1} %>%
+      max()
+    latex <- str_c(latex, "\t\t\tS[table-format = ", numCount, ".2]\n", sep = "")
+  }
+  
+  latex <- str_c(latex, "\t\t\t|}\n\t\\toprule\n\t\t", sep = "")
+  
+  if (col_count > 2) {
+    for (i in seq(1, col_count)) {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep = "")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep = "")
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep = " ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep = " ")
+    }
+  } else {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep = "")
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep = "")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count)) {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep = "")
+  
+  writeLines(latex)
+}
 
 
 vendas_media_ano 
@@ -255,6 +336,18 @@ grafico_ferraria
 ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm"
 )
 
+lojas <- data.frame(
+  Loja = c(rep("Ouro Fino", 3), rep("TendTudo", 3), rep("Ferraria Apache", 4)),
+  Produto = c("Botas de Couro", "Whisky", "Chapéu de Couro", "Espingarda", "Whisky", "Colt.45", "Chapéu de Couro", "Espingarda", "Machado", "Whisky"),
+  Quantidade = c(52, 49, 45, 53, 49, 43, 52, 42, 41, 41))
 
+colunas_bivariado <- ggplot(lojas, aes(x = Loja, y = Quantidade, fill = Produto)) +
+  geom_col(position = position_dodge(width = 0.9)) +
+  geom_text(aes(label = Quantidade),
+            position = position_dodge(width = 0.9),
+            vjust = -0.5, hjust = 0.5, size = 3.5) +
+  labs(x = "Lojas", y = "Quantidade", fill = "Produto") +
+  theme_estat()
+colunas_bivariado
 
 
